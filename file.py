d15cec22b2be24895b90a23f594cd893b7329ede
Steve6302 <shahshankstephen99@gmail.com>
2024-05-02 01:05:08 +0530

    first commit

---
+++ file.py
@@ -0,0 +1,14 @@
+## python code 
+
+
+1. print("Hello, World!")
+2. x = 5
+3. y = 10
+4. z = x + y
+5. for i in range(10):
+6.     print(i)
+7. def add_numbers(x, y):
+8.     return x + y
+  my_list = [1, 2, 3, 4, 5]
+ squared_list = [x**2 for x in my_list]
+
---
+++ readme.txt
@@ -0,0 +1,4 @@
+## just sample for the pratice
+
+
+git is tracking from this line!!
